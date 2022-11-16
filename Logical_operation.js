95486b4f6f0764472fbf9e8b8e204913b13c12dc
giorgio <giorgio.programmer@gmail.com>
2022-11-16 21:31:12 +0100

    Create Logical_operation.js

---
+++ Logical_operation.js
@@ -0,0 +1,37 @@
+// first exercise 
+
+function orderMyLogic(val) {
+  if (val < 10) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// second exercise 
+
+orderMyLogic(7);
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+
+// third exercise 
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
