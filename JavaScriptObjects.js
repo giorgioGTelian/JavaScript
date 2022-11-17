625ba1731ba9ba894c4096efce585a7268aa5332
giorgio <giorgio.programmer@gmail.com>
2022-11-17 23:16:44 +0100

    Create JavaScriptObjects.js

---
+++ JavaScriptObjects.js
@@ -0,0 +1,28 @@
+const cat = {
+  "name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+}; 
+
+//In this example, all the properties are stored as strings, such as name, legs, and tails. 
+//However, you can also use numbers as properties.
+//You can even omit the quotes for single-word string properties, as follows:
+
+const anotherObject = {
+  make: "Ford",
+  5: "five",
+  "model": "focus"
+};
+
+// However, if your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+const myDog = {
+
+name: "luk",
+legs: 9,
+tails: 4,
+friends: ["charlie", "alfredo"]
+
+
+};
